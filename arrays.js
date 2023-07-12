0c7e2eedb7e62ff041e6abb0c9819ba86d795cff
Priyanshu2908 <prianshuchaubey29@gmail.com>
2023-07-12 13:36:16 +0530

    commit

--- arrays.js
+++ arrays.js
@@ -59,3 +59,35 @@ for(let n of nums){
 }
 
 console.log(newNums);
+
+//using Map function.
+console.log('-----Using Map Function----');
+const result = nums.map((n) => {return n**2});
+console.log(result);
+
+const prices=[1329, 38267, 4002, 402, 5793, 352];
+console.log(1329 * 0.05+1329);
+
+//use map function to add 5% gst to all prices.
+
+const newprices= prices.map((p)=> {return p * 0.05 + p})
+console.log(newprices);
+
+
+
+const prices2=['$2423.23','$583.39','$63.99','23.99','$12.9','$900.99'];
+console.log(parseInt('$2433.23'.slice(1)));
+const numprices= prices2.map((p)=> parseInt(p.slice(1)));
+console.log(numprices);
+
+//using Filter Function
+const filteredPrices= prices.filter((p)=>{return p>=500 && p<=5000});
+console.log(filteredPrices);
+
+const laptops=['Dell Vostro', 'Acer Predator', 'Asus vivobook', 'HP Pavillion', 'Lenevo Thinkpad', 'Apple macbook'];
+
+console.log('Dell Vostro'.includes('dell'));
+const keyword='Asus';
+const searchData = laptops.filter((title)=> {return title.toLowerCase().includes(keyword.toLowerCase())});
+console.log(searchData);
+
